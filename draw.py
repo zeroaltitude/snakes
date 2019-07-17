627ecae403490c0a9ebad03f417ec3b8de58e8c0
Edward Abrams <eabrams@godaddy.com>
2019-07-17 16:23:01 -0700

    abstract out bits and grid and improve dramatically the rendering

--- draw.py
+++ draw.py
@@ -5,37 +5,22 @@ from PIL import Image, ImageDraw
 
 
 MODE = "RGBA"
-SIZE = (26, 26)
 BGCOLOR = "#FFFFFF"
 DOTCOLOR = "#000000"
 LINECOLOR = "#111111"
 DARK = "#000000"
-GRID = (6, 6)
-BITS = 60
-BIT_GRID = [
-      (0, 1),   (1, 2),   (2, 3),   (3, 4),   (4, 5),   (0, 6),   (1, 7),   (2, 8),   (3, 9),  (4, 10),  (5, 11),
-      (6, 7),   (7, 8),   (8, 9),  (9, 10), (10, 11),  (6, 12),  (7, 13),  (8, 14),  (9, 15), (10, 16), (11, 17),
-    (12, 13), (13, 14), (14, 15), (15, 16), (16, 17), (12, 18), (13, 19), (14, 20), (15, 21), (16, 22), (17, 23),
-    (18, 19), (19, 20), (20, 21), (21, 22), (22, 23), (18, 24), (19, 25), (20, 26), (21, 27), (22, 28), (23, 29),
-    (24, 25), (25, 26), (26, 27), (27, 28), (29, 29), (24, 30), (25, 31), (26, 32), (27, 33), (28, 34), (29, 35),
-    (30, 31), (31, 32), (32, 33), (33, 34), (34, 35)
-]
-DOTS = []
-DOTRADIUS = 1
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
           103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
-CHARS = []
-LINES = []
 
 
 def draw_dots(draw, offset_x=0, offset_y=0):
     DOTS.clear()
-    for y in range(GRID[1]):
-        for x in range(GRID[0]):
-            gridx = (x * (SIZE[0] - 1) // (GRID[0] - 1)) - x + offset_x
-            gridy = (y * (SIZE[1] - 1) // (GRID[1] - 1)) - y + offset_y
-            # print((gridx, gridy, gridx + DOTRADIUS, gridy + DOTRADIUS))
-            draw.ellipse((gridx, gridy, gridx + DOTRADIUS, gridy + DOTRADIUS), fill=DOTCOLOR, outline=DARK)
+    for y in range(GRID):
+        for x in range(GRID):
+            distance = (SIZE - 1) / (GRID - 1)
+            gridx = (x * distance) + offset_x
+            gridy = (y * distance) + offset_y
+            draw.point((gridx, gridy), fill=DOTCOLOR)
             DOTS.append([gridx, gridy])
 
 
@@ -49,14 +34,14 @@ def draw_lines(draw, snake_chars):
             gridbx = DOTS[selected[1]][0]
             gridby = DOTS[selected[1]][1]
             xy = (gridax, griday, gridbx, gridby)
-            print("guessing coords %s, %s, %s, %s\n" % xy)
+            # print("guessing coords %s, %s, %s, %s\n" % xy)
             draw.line(xy, fill=LINECOLOR, width=1, joint=None)
 
 
 def single_image_output(snake_chars, snake_id, trans=False):
     snake_png = "snake_%s.png" % snake_id
-    print("snake img %s" % snake_png)
-    im = init_image(MODE, SIZE, BGCOLOR, trans=trans)
+    # print("snake img %s" % snake_png)
+    im = init_image(MODE, [SIZE, SIZE], BGCOLOR, trans=trans)
     draw = get_draw(im)
     draw_dots(draw)
     # print("snake chars to draw is: %s" % snake_chars)
@@ -73,19 +58,19 @@ def append_output(snake_chars, offset_x, offset_y, canvas_draw):
 def get_next_dimensions(total_word_length, can_x, snake_id):
     page_margin_x = 2
     page_margin_y = 2
-    vertical_line_margin = 5
-    horizontal_char_margin = 3
-    horizontal_word_margin = 12
+    vertical_line_margin = 10
+    horizontal_char_margin = 5
+    horizontal_word_margin = 10
     # x props
     num_spaces = len([c for c in CHARS if c == 0])
     num_chars = len([c for c in CHARS if c == 1])
     start_position_x = page_margin_x + (num_spaces * horizontal_word_margin) + (
-                num_chars * (SIZE[0] + horizontal_char_margin))
-    word_len_x = total_word_length * (SIZE[0] + horizontal_char_margin)
-    print("page_margin_x: %s; page_margin_y: %s; vertical_line_margin: %s; horizontal_char_margin: %s; horizontal_word_margin: %s" % (page_margin_x, page_margin_y, vertical_line_margin, horizontal_char_margin, horizontal_word_margin))
-    print("num_spaces: %s; num_chars: %s; start_position_x: %s; word_len_x: %s" % (num_spaces, num_chars, start_position_x, word_len_x))
+                num_chars * (SIZE + horizontal_char_margin))
+    word_len_x = total_word_length * (SIZE + horizontal_char_margin)
+    # print("page_margin_x: %s; page_margin_y: %s; vertical_line_margin: %s; horizontal_char_margin: %s; horizontal_word_margin: %s" % (page_margin_x, page_margin_y, vertical_line_margin, horizontal_char_margin, horizontal_word_margin))
+    # print("num_spaces: %s; num_chars: %s; start_position_x: %s; word_len_x: %s" % (num_spaces, num_chars, start_position_x, word_len_x))
     if snake_id == 0:
-        print("snake 0")
+        # print("snake 0")
         # do we have enough space in this line to write this whole word; if not, advance now to next line
         if (start_position_x + word_len_x) >= (can_x + page_margin_x):
             # new line
@@ -95,13 +80,13 @@ def get_next_dimensions(total_word_length, can_x, snake_id):
             num_spaces = len([c for c in CHARS if c == 0])
             num_chars = len([c for c in CHARS if c == 1])
             start_position_x = page_margin_x + (num_spaces * horizontal_word_margin) + (
-                        num_chars * (SIZE[0] + horizontal_char_margin))
-    offset_y = page_margin_y + (len(LINES) * (SIZE[1] + vertical_line_margin))
+                        num_chars * (SIZE + horizontal_char_margin))
+    offset_y = page_margin_y + (len(LINES) * (SIZE + vertical_line_margin))
     offset_x = start_position_x
     CHARS.append(1)
     if snake_id == (total_word_length - 1):
         # append a space
-        print("append space")
+        # print("append space")
         CHARS.append(0)
     return offset_x, offset_y
 
@@ -134,7 +119,7 @@ def draw_snakes_main(words, trans=False, single_word_images=True, can_x=0, can_y
     canvas_draw = None
     can_im = None
     if single_word_images is False:
-        print("dsm: single word")
+        # print("dsm: single word")
         can_im = init_image(MODE, [can_x, can_y], BGCOLOR, trans=trans)
         canvas_draw = get_draw(can_im)
     for word in words.split():
@@ -174,15 +159,38 @@ def main(word, single_word_images, can_x, can_y, trans):
     else:
         trans = False
     if single_word_images == 1:
-        print("single word images!")
+        # print("single word images!")
         single_word_images = True
     else:
-        print("single big canvas!")
+        # print("single big canvas!")
         single_word_images = False
     draw_snakes_main(word, trans=trans, single_word_images=single_word_images, can_x=can_x, can_y=can_y)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
-        raise Exception("draw.py word single_word_images(1|0) can_x(0|255) can_y(0|255) trans(0|1)")
-    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
+    if len(sys.argv) != 7:
+        raise Exception("draw.py word single_word_images(1|0) can_x(0|255) can_y(0|255) trans(0|1) grid(6|x)")
+
+    word, single_word_images, can_x, can_y, trans, GRID = sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6])
+
+    BITS = GRID * (GRID - 1) * 2
+    DISTANCE = 3
+    SIZE = (GRID - 1) * (DISTANCE) + 1
+
+    BIT_GRID = []
+    for i in range(GRID * 2 - 1):
+        linebase = (i // 2) * GRID
+        if i % 2 == 0:
+            # output a "horizontal" point grid of size n - 1
+            for j in range(GRID - 1):
+                BIT_GRID.append((linebase + j, linebase + j + 1));
+        else:
+            # output a "vertical" point grid of size n
+            for j in range(GRID):
+                BIT_GRID.append((linebase + j, linebase + j + GRID))
+    # print(BITS)
+    DOTS = []
+    CHARS = []
+    LINES = []
+
+    main(word, single_word_images, can_x, can_y, trans)
