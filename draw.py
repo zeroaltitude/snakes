ea0abc5d646c95539b93a0cbaa037dbdb635e4e0
eabrams <eabrams@godaddy.com>
2019-07-20 15:29:15 -0700

    *nix, color

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw
 
 MODE = "RGBA"
 BGCOLOR = "#FFFFFF"
-DOTCOLOR = "#000000"
+DOTCOLOR = "#ddd"
 LINECOLOR = "#111111"
 DARK = "#000000"
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
